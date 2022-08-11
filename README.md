987ed3f0203b73174b6ddca2e4cbc5dbfc914e35
Erwin Méndez <erwinmendez0911@hotmail.com>
2022-08-10 20:46:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Project Starter - Vite + ReactJS + TypeScript
+
+Project starter for ReactJS app using:
+
+- Vite
+- TypeScript
+- Tailwind CSS
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="max-w-md mx-auto p-5">
+      <h1 className="text-2xl font-bold text-center">Hello there!</h1>
+    </div>
+  );
+};
+
+export default App;
